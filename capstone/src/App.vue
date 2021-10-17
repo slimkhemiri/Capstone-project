3353c38c656a530634e94933343d4cfb1c04f9a8
slimkhemiri <khemiri.medslim1@gmail.com>
2021-10-17 23:25:09 +0100

    clear the code

--- capstone/src/App.vue
+++ capstone/src/App.vue
@@ -2,42 +2,77 @@
 import QandA from "./components/Q&A.vue";
 import searchBar from "./components/Search.vue";
 import moreAnswers from "./components/moreAnswers.vue";
-import Rating from "./components/rating.vue";
-import HorizontalListRelated from "./components/HorizontalListRelated.vue"
-import HorizontalListYourOutfit from "./components/HorizontalListYourOutfit.vue"
+import Rate from "./components/rate.vue";
+import HorizontalListRelated from "./components/HorizontalListRelated.vue";
+import HorizontalListYourOutfit from "./components/HorizontalListYourOutfit.vue";
+import Style from "./components/Circle.vue";
+import Tiltle from "./components/Tiltle.vue";
+import NavBar from "./components/navbar.vue";
+import Caroussel from "./components/caroussel.vue";
+import RatingF from "./components/RatingF.vue";
+import Product from "./components/ProducetPrice.vue";
+import Select from "./components/Select.vue";
+import Add from "./components/Add.vue";
 export default {
-
   components: {
     QandA: QandA,
     searchBar: searchBar,
     moreAnswers: moreAnswers,
-    Rating: Rating,
-      HorizontalListRelated :HorizontalListRelated,
-      HorizontalListYourOutfit:HorizontalListYourOutfit,
+    Rate: Rate,
+    HorizontalListRelated: HorizontalListRelated,
+    HorizontalListYourOutfit: HorizontalListYourOutfit,
+    NavBar,
+    Tiltle,
+    Caroussel,
+    RatingF,
+    Product,
+    Style,
+    Select,
+    Add,
   },
-   computed: {
-     products() {
-       return this.$store.getters.products;
-     },
-   },
-   created() {
-     this.$store.dispatch("loadProducts");
-   },
+  computed: {
+    products() {
+      return this.$store.getters.products;
+    },
+  },
+  created() {
+    this.$store.dispatch("loadProducts");
+  },
 };
 </script>
 <template>
   <div id="app" class=" container-center ">
-    
     <div>
-      RELATED PRODUCT
-     <HorizontalListRelated/>
-     YOUR OUTFIT
-     <HorizontalListYourOutfit/>
-      <searchBar/>
-    <QandA  /> 
-    <moreAnswers/>
-    <Rating/>
- 
-  </div>
+      <div>
+        <NavBar />
+
+        <Tiltle />
+
+        <div class="grid grid-rows-4 grid-flow-col gap-4 mx-20 heightt margin">
+          <Caroussel />
+          <RatingF />
+          <Product />
+          <div class="row-span-1 w-96 col-span-1  h -mt-16 ">
+            <div class="ml-4">STYLE > SLECTED STYLE</div>
+            <Style />
+          </div>
+
+          <div class="row-span-1 col-span-1  float-left het">
+            <Select />
+
+            <div class="row-span-1 col-span-1  mt-2 heet ">
+              <Add />
+            </div>
+          </div>
+        </div>
+      </div>
+      <HorizontalListRelated />
+
+      <HorizontalListYourOutfit />
+      <searchBar />
+      <QandA />
+      <moreAnswers />
+      <Rate />
+    </div>
   </div>
 </template>
---
+++ capstone/src/components/Tiltle.vue
@@ -0,0 +1,35 @@
+<template>
+        <div class="container mx-auto grid grid-rows-6 min-h-screen mx-20 margin1">
+      <div class="row-span-1 text-center -mt-8 h-8 color">
+        SITE-WIDE ANNOUNCEMENT MESSAGE! -- SALE / DICOUNT
+        <span class="font-bold cl"> OFFER </span> --<span class="underline">
+          NEW PRODUCT HIGHLIGHT</span
+        >
+      </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:"Tiltle",
+    data:()=>({
+
+    })
+    
+
+}
+</script>
+
+<style lang="scss">
+.margin1 {
+  margin-top: -40%;
+}
+.color {
+  color: darkgrey;
+}
+.cl {
+  color: rgb(61, 44, 44);
+}
+
+</style>
